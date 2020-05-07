e8e62fcda53faad3ecc01cd399ef7bdc74620782
Matteo De Leo <matteodeleo89@live.com>
2020-05-07 15:18:56 +0200

    finished

--- script.sh
+++ script.sh
@@ -7,11 +7,17 @@ if [ -d "$DIR" ]; then
   ### Take action if $DIR exists ###
   echo "Switching to ${DIR}..."
   cd $DIR
-  git status
+  if [ -d .git ]; then
+    git status
+  else
+    git init
+    git add --all
+    git commit -m "initial Commit - directory under surveillance"
+  fi
   exit
 else
   ###  Control will jump here if $DIR does NOT exists ###
-  echo "${DIR} not found. Creating..."
+  echo -n "${DIR} not found. Creating...\n"
   mkdir $DIR
   cd $DIR && git init
   exit
